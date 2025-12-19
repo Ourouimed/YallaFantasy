89f7817f8f412cd2333eaad24a9eb3fd7e1e39cd
= <=>
2025-12-19 20:19:32 +0100

    dddd

--- backend/middelware/corsOptions.js
+++ backend/middelware/corsOptions.js
@@ -1,6 +1,5 @@
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' ? (process.env.ALLOW_CORS_URL || 'https://your-frontend-domain.vercel.app') : 'http://localhost:3000',
-
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
